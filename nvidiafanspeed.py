a100144bf500effe59f4a274decf89d2995486e4
Claudio Pupparo <claudio.pupparo@gmail.com>
2015-06-21 11:12:52 +0200

    From driver version 352.09 on, a different attribute for fan control must be used In driver versions 349.16 and 349.12 there is a regression that makes impossibile to control fan speed Thanks Ricardo Platao for the tip :D

--- nvidiafanspeed.py
+++ nvidiafanspeed.py
@@ -56,7 +56,7 @@ ensure that gradient is always positive and less than infinity
 current temperature = 12
 
 curve point = [temp, speed]
-curve point 1 = [10, 5] 
+curve point 1 = [10, 5]
 curve point 2 = [20, 10]
 
 curve point 1 temperature (10) <= current temperature (12) < curve point 2 temperature (20)
@@ -66,14 +66,14 @@ gradient = (10 - 5)/(20 - 10) = 0.5 #from point 1 to point 2, fan speed must be 
 difference current temperature-previously point temperature = 12 - 10 = 2
 
 new speed = 5 + 2*0.5 = 6
-"""	
+"""
 
 class Curve():
 
 	def __init__(self, *args, **kwargs):
 		if len(args) == 1: #[[temp0, speed0], [temp1, speed1], [temp2, speed2]]
 			self.cpa = list(args[0])
-		if len(args) == 2: #[[temp0, temp1, temp2], [speed0, speed1, speed2]]			
+		if len(args) == 2: #[[temp0, temp1, temp2], [speed0, speed1, speed2]]
 			self.convertIntoMatrix(args[0], args[1])
 
 	def convertIntoMatrix(self, x_values, y_values):
@@ -101,13 +101,13 @@ class Curve():
 				return y
 
 			point_i += 1
- 	 	        
+
 	def setCurve(self, *args, **kwargs):
 		if len(args) == 1:
 			self.cpa = list(args[0])
 		if len(args) == 2:
 			self.convertIntoMatrix(args[0], args[1])
-		
+
 
 class StoppableThread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
@@ -124,12 +124,16 @@ class StoppableThread(threading.Thread):
         return self._stop.isSet()
 
 class NvidiaFanController(StoppableThread):
+
+	DRIVER_VERSION_CHANGE = 352.09 #from this version on, we need to use a different method to change fan speed
+	DRIVER_VERSIONS_ERROR = [349.16, 349.12] #cannot control fan speed in these driver versions
+
 	"""Room here for arguments to implement multigpu fan control"""
 	def __init__(self, *args, **kwargs):
 		super(NvidiaFanController, self).__init__()
-		signal.signal(signal.SIGINT, self.exit_signal_handler) #CTRL-C 
+		signal.signal(signal.SIGINT, self.exit_signal_handler) #CTRL-C
 		signal.signal(signal.SIGQUIT, self.exit_signal_handler) #CTRL-\
-		signal.signal(signal.SIGHUP, self.exit_signal_handler) #terminal closed		
+		signal.signal(signal.SIGHUP, self.exit_signal_handler) #terminal closed
 		signal.signal(signal.SIGTERM, self.exit_signal_handler)
 		self.curve_lock = threading.Lock()
 
@@ -140,7 +144,7 @@ class NvidiaFanController(StoppableThread):
 
 	def exit_signal_handler(self, signal, frame):
 		self.stop()
-	
+
 	def stop(self):
 		super(NvidiaFanController, self).stop() #StoppableThread.stop()
 
@@ -152,7 +156,7 @@ class NvidiaFanController(StoppableThread):
 			#print "Custom Fan Speed pid ", os.getpid()
 			#for index in range(0, len(self.curve.cpa)):
 			#	print self.curve.cpa[index]
-			
+
 			current_temp = self.getTemp()
 			new_fan_speed = self.curve.evaluate(current_temp)
 			#os.system("clear")
@@ -162,29 +166,42 @@ class NvidiaFanController(StoppableThread):
 			time.sleep(1.0)
 
 			self.curve_lock.release()
-			
+
 		self.resetFan()
-		
+
 		print "Exit"
 		#finished and ready to exit
 		return
-	
+
 	def resetFan(self):
 		print "Reset to Auto Fan"
 		process = Popen("nvidia-settings -a [gpu:0]/GPUFanControlState=0", shell=True, stdin=PIPE, stdout=PIPE)
-	
+
 	def getTemp(self):
 		process = Popen("nvidia-settings -q gpucoretemp", shell=True, stdin=PIPE, stdout=PIPE)
 		line_array = process.stdout.readlines()
 		tmp_line = line_array[1]
 		#grab number from end of line
 		return int(tmp_line[-4:-2])
-	
+
 	def setFanSpeed(self, speed):
-        #watch -n0 aticonfig --adapter=0 --od-gettemperature
-        #that's the line to get ati fan speed
-		process = Popen("nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUCurrentFanSpeed={0}".format(speed), shell=True, stdin=PIPE, stdout=PIPE)
-		return
+		drv_ver = self.getDriverVersion()
+
+		if drv_ver >= NvidiaFanController.DRIVER_VERSION_CHANGE:
+			process = Popen("nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed={0}".format(speed), shell=True, stdin=PIPE, stdout=PIPE)
+		elif drv_ver in NvidiaFanController.DRIVER_VERSIONS_ERROR:
+			print "Cannot control fan speed in version {0} of the driver".format(drv_ver)
+		else:
+			process = Popen("nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUCurrentFanSpeed={0}".format(speed), shell=True, stdin=PIPE, stdout=PIPE)
+
+	def getDriverVersion(self):
+		try:
+			out = check_output(["nvidia-settings", "-q", "[gpu:0]/NvidiaDriverVersion"])
+			drv_ver = float(out[out.rfind(":")+1:].replace("\n", "").strip())
+			return drv_ver
+		except Exception:
+			print "Exception in parsing Nvidia Driver Version. We'll use old attribute 'GPUCurrentFanSpeed'"
+			return -1
 
 	def setCurve(self, *args, **kwargs):
 		self.curve_lock.acquire()
@@ -195,7 +212,7 @@ class NvidiaFanController(StoppableThread):
 
 		if len(args) == 2:
 			self.curve.setCurve(args[0], args[1])
-		
+
 		self.curve_lock.release()
 
 
@@ -210,7 +227,7 @@ if __name__ == "__main__":
 
 	y_speed = list()
 	for index in range(0, len(curve_point_array)):
-		y_speed.append(curve_point_array[index][1])	
+		y_speed.append(curve_point_array[index][1])
 
 	#nvidiaController = NvidiaFanController(curve_point_array)
 	nvidiaController = NvidiaFanController(x_temp, y_speed)
